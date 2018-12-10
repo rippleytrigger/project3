b6e7dde4cec023a5f7151e61996528ca7b7e7bf5
Marcos Lopez <rippleytrigger@gmail.com>
2018-12-10 13:18:07 -0400

    I want to advise the user of the app of the exceptions of the register module

--- users/views.py
+++ users/views.py
@@ -18,24 +18,43 @@ def index(request):
     return render(request, "users/user.html", context)
 
 def login(request):
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        auth_login(request, user)
-        return HttpResponseRedirect(reverse("index"))
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/login.html", {"message": "Invalid credentials."})
     else:
-        return render(request, "users/login.html", {"message": "Invalid credentials."})
+        return render(request, "users/login.html")
 
 def logout(request):
     auth_logout(request)
     return render(request, "users/login.html", {"message": "Logged out."})
 
 def signup(request):
-    username = request.POST["username"]
-    password = request.POST["password"]
-    first_name = request.POST["first_name"]
-    last_name = request.POST["last_name"]
-    email = request.POST["email"]
 
-    return render(request, 'users/sigup.html')
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        email = request.POST["email"]
+
+        try:
+            user = User.objects.create_user(username = username, password = password, first_name = first_name, last_name = last_name, email = email)
+        except ValueError:
+            return render(request, 'users/sigup.html', {"message": Exception})
+
+        user.save()
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+
+    else:
+        return render(request, 'users/sigup.html')
